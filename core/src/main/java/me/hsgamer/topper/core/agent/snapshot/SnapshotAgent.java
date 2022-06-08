6a0adf383c81e034e822a5be6e31e5b1997ada3a
HSGamer <huynhqtienvtag@gmail.com>
2022-06-08 09:46:53 +0700

    filters for SnapshotAgent

--- core/src/main/java/me/hsgamer/topper/core/agent/snapshot/SnapshotAgent.java
+++ core/src/main/java/me/hsgamer/topper/core/agent/snapshot/SnapshotAgent.java
@@ -6,6 +6,7 @@ import me.hsgamer.topper.core.holder.DataHolder;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,6 +14,7 @@ public class SnapshotAgent<T, R> extends TaskAgent<R> {
     private final AtomicReference<List<DataSnapshot<T>>> topSnapshot = new AtomicReference<>(Collections.emptyList());
     private final AtomicReference<Map<UUID, Integer>> indexMap = new AtomicReference<>(Collections.emptyMap());
     private final DataHolder<T> holder;
+    private final List<Predicate<UUID>> filters = new ArrayList<>();
     private Comparator<T> comparator;
 
     public SnapshotAgent(DataHolder<T> holder) {
@@ -23,6 +25,7 @@ public class SnapshotAgent<T, R> extends TaskAgent<R> {
     protected Runnable getRunnable() {
         return () -> {
             List<DataSnapshot<T>> list = holder.getEntryMap().entrySet().stream()
+                    .filter(entry -> filters.parallelStream().allMatch(filter -> filter.test(entry.getKey())))
                     .map(entry -> new DataSnapshot<>(entry.getKey(), entry.getValue().getValue()))
                     .sorted(Comparator.<DataSnapshot<T>, T>comparing(DataSnapshot::getValue, comparator).reversed())
                     .collect(Collectors.toList());
@@ -67,5 +70,9 @@ public class SnapshotAgent<T, R> extends TaskAgent<R> {
 
     public void setComparator(Comparator<T> comparator) {
         this.comparator = comparator;
+    }
+
+    public void addFilter(Predicate<UUID> filter) {
+        filters.add(filter);
     }
 }
