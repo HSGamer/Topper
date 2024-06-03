4ff1e06bf7af9f1a232cb3c3dc58451fd58a67bd
hsgamer <huynhqtienvtag@gmail.com>
2024-06-03 21:53:05 +0700

    update the update version script

--- update_versions.sh
+++ update_versions.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+UPDATE_DIR="
+agent
+spigot
+"
+
+BASE_DIR="$(pwd)"
 mvn -N versions:update-child-modules -DgenerateBackupPoms=false
+for dir in $UPDATE_DIR; do
+  cd "$BASE_DIR/$dir" || exit
+  mvn -N versions:update-child-modules -DgenerateBackupPoms=false
+done
+cd "$BASE_DIR" || exit
