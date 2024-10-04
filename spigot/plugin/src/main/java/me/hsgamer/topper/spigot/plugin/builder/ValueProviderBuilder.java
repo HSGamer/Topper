1747f64f758fd8cce8d08f985024b1efd7f5587d
hsgamer <huynhqtienvtag@gmail.com>
2024-10-04 23:47:09 +0700

    add StatisticValueProvider

--- spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/builder/ValueProviderBuilder.java
+++ spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/builder/ValueProviderBuilder.java
@@ -1,12 +1,17 @@
 package me.hsgamer.topper.spigot.plugin.builder;
 
 import me.hsgamer.hscore.builder.FunctionalMassBuilder;
+import me.hsgamer.topper.spigot.plugin.provider.StatisticValueProvider;
 import me.hsgamer.topper.spigot.plugin.provider.ValueProvider;
 
 import java.util.Map;
 import java.util.Objects;
 
 public class ValueProviderBuilder extends FunctionalMassBuilder<Map<String, Object>, ValueProvider> {
+    public ValueProviderBuilder() {
+        register(StatisticValueProvider::new, "statistic", "stat");
+    }
+
     @Override
     protected String getType(Map<String, Object> map) {
         return Objects.toString(map.get("type"), "");
---
+++ spigot/plugin/src/main/java/me/hsgamer/topper/spigot/plugin/provider/StatisticValueProvider.java
@@ -0,0 +1,72 @@
+package me.hsgamer.topper.spigot.plugin.provider;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Statistic;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+public class StatisticValueProvider implements ValueProvider {
+    private final Statistic statistic;
+    private final Material material;
+    private final EntityType entityType;
+
+    public StatisticValueProvider(Map<String, Object> map) {
+        this.statistic = Optional.ofNullable(map.get("statistic"))
+                .map(Objects::toString)
+                .map(String::toUpperCase)
+                .flatMap(s -> {
+                    try {
+                        return Optional.of(Statistic.valueOf(s));
+                    } catch (IllegalArgumentException e) {
+                        return Optional.empty();
+                    }
+                })
+                .orElse(null);
+        this.material = Optional.ofNullable(map.get("material"))
+                .map(Objects::toString)
+                .map(Material::matchMaterial)
+                .orElse(null);
+        this.entityType = Optional.ofNullable(map.get("entity"))
+                .map(Objects::toString)
+                .map(String::toUpperCase)
+                .flatMap(s -> {
+                    try {
+                        return Optional.of(EntityType.valueOf(s));
+                    } catch (IllegalArgumentException e) {
+                        return Optional.empty();
+                    }
+                })
+                .orElse(null);
+    }
+
+    @Override
+    public CompletableFuture<Optional<Double>> getValue(UUID uuid) {
+        return CompletableFuture.supplyAsync(() -> {
+            Player player = Bukkit.getPlayer(uuid);
+            if (player == null) {
+                return Optional.empty();
+            }
+
+            if (statistic == null) {
+                return Optional.empty();
+            }
+
+            switch (statistic.getType()) {
+                case BLOCK:
+                case ITEM:
+                    return Optional.ofNullable(material).map(m -> (double) player.getStatistic(statistic, m));
+                case ENTITY:
+                    return Optional.ofNullable(entityType).map(e -> (double) player.getStatistic(statistic, e));
+                default:
+                    return Optional.of((double) player.getStatistic(statistic));
+            }
+        });
+    }
+}
